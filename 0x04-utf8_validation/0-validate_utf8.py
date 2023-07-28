719cb3ae7db2301693ee5c725dd5fd19bbd46593
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-07-28 10:09:38 +0000

    Recode everything

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -9,25 +9,19 @@ def validUTF8(data):
     determines if a given data set represents a valid UTF-8 encoding
     returns true if it is else false
     """
-    count = 0
-    for num in data:
-        # If the count is 0, check how many leading 1's
-        # there are in the current integer
-        if count == 0:
-            if (num >> 5) == 0b110:
-                count = 1
-            elif (num >> 4) == 0b1110:
-                count = 2
-            elif (num >> 3) == 0b11110:
-                count = 3
-            elif (num >> 7) != 0:
+    num_bytes = 0
+    for byte in data:
+        mask = 1 << 7
+        if not num_bytes:
+            while byte & mask:
+                num_bytes += 1
+                mask >>= 1
+            if not num_bytes:
+                continue
+            if num_bytes == 1 or num_bytes > 4:
                 return False
-        # else check if the current integer is a continuation byte
         else:
-            if (num >> 6) != 0b10:
+            if byte >> 6 != 0b10:
                 return False
-            count -= 1
-
-    # If the count is still not 0 after iterating
-    # through all the integers, it is invalid
-    return count == 0
+        num_bytes -= 1
+    return num_bytes == 0
