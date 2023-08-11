10c306e67753f6059d4ec507ee1d776ab4ac693c
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-08-11 05:20:54 +0000

    Make semi-standart compliant

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,29 +2,29 @@
 const request = require('request');
 
 const movieId = process.argv[2];
-const url = "https://swapi-api.alx-tools.com/api/films/" + movieId;
+const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-function requestActors(actors, index) {
-    if (actors.length === index) {
-        return;
+function requestActors (actors, index) {
+  if (actors.length === index) {
+    return;
+  }
+
+  request(actors[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      requestActors(actors, index + 1);
     }
-
-    request(actors[index], (error, response, body) => {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log(JSON.parse(body).name);
-            requestActors(actors, index + 1);
-        }
-    });
+  });
 }
 
 request(url, (error, response, body) => {
-    if (error) {
-        console.log(error);
-    } else {
-        const actors = JSON.parse(body).characters;
+  if (error) {
+    console.log(error);
+  } else {
+    const actors = JSON.parse(body).characters;
 
-        requestActors(actors, 0);
-    }
+    requestActors(actors, 0);
+  }
 });
