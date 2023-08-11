685593ac443e4410f7193068758254fe70f6f051
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-08-11 05:17:34 +0000

    Updte code

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,33 +1,30 @@
 #!/usr/bin/node
-
 const request = require('request');
 
 const movieId = process.argv[2];
 const url = "https://swapi-api.alx-tools.com/api/films/" + movieId;
 
+function requestActors(actors, index) {
+    if (actors.length === index) {
+        return;
+    }
+
+    request(actors[index], (error, response, body) => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(JSON.parse(body).name);
+            requestActors(actors, index + 1);
+        }
+    });
+}
+
 request(url, (error, response, body) => {
     if (error) {
         console.log(error);
     } else {
+        const actors = JSON.parse(body).characters;
 
-        const charactersUrls = JSON.parse(body).characters;
-
-        if (charactersUrls.length === 0) {
-            return;
-        }
-
-        // send another request to get actors names
-        for (i = 0; i < charactersUrls.length; i++)
-        {
-            const actorUrl = charactersUrls[i];
-            request(actorUrl, (error, res,  body1) => {
-                if (error) {
-                    console.log(error);
-                } else {
-                    const name = JSON.parse(body1).name;
-                    console.log(name);
-                }
-            });
-        }
+        requestActors(actors, 0);
     }
 });
